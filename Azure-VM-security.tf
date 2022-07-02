0675ed44601f92853d8fe92bb0844f9e549ca2af
anandvamsi <anandvamsi.v@gmail.com>
2022-07-02 23:54:14 +0530

    Create Azure-VM-security.tf

---
+++ Azure-VM-security.tf
@@ -0,0 +1,17 @@
+resource "azurerm_network_security_group" "nsg" {
+  name                = "ssh_nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "allow_ssh_sg"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
